8104f84b6137e77ab025e74174e7221c907a6151
Karim B <mattiakarim@live.it>
2019-06-03 14:36:24 +0200

    Add comments for each post and input field for each

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -7,17 +7,32 @@ class App extends Component {
     super();
     this.state = {
       instaData: dummyData,
+      newComment: '',
     }
   }
+
+  inputChangeHandler = event => {
+    this.setState({
+      newComment: event.target.value,
+    })
+  } 
 
 
   render() {
     return (
       <div className="App">
         hello world
-  
+        {
+          this.state.instaData.map(dataObj => {
+            return <PostContainer 
+                      key={dataObj.id} 
+                      data={dataObj}
+                      value={this.state.newComment}
+                      changes={this.inputChangeHandler}/>
+          })        
+        }
         {/* <SearchBar />
-        <PostContainer />
+        
         <CommentSection /> */}
       </div>
     );
@@ -30,11 +45,40 @@ class App extends Component {
 //   return();
 // }
 
-// function PostContainer(props) {
-//   return();
-// }
+function PostContainer(props) {
+  return(
+    <div>
+      {/* {
+        props.data.map()
+      } */}
+      <CommentSection 
+        comments={props.data.comments} 
+        value={props.value}
+        changes={props.changes}/>
+    </div>
+  );
+}
 
-// function CommentSection(props) {
-//   return();
-// }
+function CommentSection(props) {
+  return(
+    <div>
+      {
+        props.comments.map(comment => {
+          return(
+              <p className="comment-section" 
+                  key={comment.id}>
+                    <span className="username"
+                    >{comment.username}</span>
+                  {comment.text}</p>
+          )
+        })
+      }
+      <input 
+        type="text" 
+        value={props.value}
+        onChange={props.changes} 
+        placeholder="Add a comment ..."/> 
+    </div>
+  );
+}
 export default App;
