e1b1d0967130c78cb6d51871a9102c5ab45a06bf
Karim B <mattiakarim@live.it>
2019-06-05 15:29:12 +0200

    Add styling to the login page

--- instagram/src/components/Login/LoginPage.jsx
+++ instagram/src/components/Login/LoginPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './LoginPage.css';
 
 function LoginPage(props) {
     return (
-        <div>
-            <input 
-                type="text" 
-                className="login-username" 
-                onChange={props.userInputHandler}/>
-            <input 
-                type="password" 
-                className="login-passWord" />
+        <div className="login-wrapper">
+            <div className="username-wrapper">
+                <label>Username</label>
+                <input 
+                    type="text" 
+                    className="login-username" 
+                    onChange={props.userInputHandler}/>
+            </div>
+            <div className="password-wrapper">
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    className="login-passWord" />
+            </div>
             <button onClick={props.onClick}>Login</button>
         </div>
     );
